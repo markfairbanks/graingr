5a5f42338f36365e0fb1b91d5703100b470f0a76
Mark Fairbanks <Mark.Fairbanks@grainger.com>
2019-10-24 13:12:55 -0500

    Update docs

--- R/inv_gc.R
+++ R/inv_gc.R
@@ -1,3 +1,12 @@
+#' Run invisible garbage collection
+#'
+#' Run garbage collection without the typical output.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' inv_gc()
 inv_gc <- function() {
   invisible(gc())
 }
